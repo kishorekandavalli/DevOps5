4485c3d1eed60255950878b761f254d218ff83c0
kishorekandavalli <34689881+kishorekandavalli@users.noreply.github.com>
2019-05-03 16:57:16 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -87,24 +87,6 @@ else
 fi
 
 
-## Downloading SonarQube 
-#VER=$(curl -s https://sonarsource.bintray.com/Distribution/sonarqube/  | tail -n 10 | awk -F '[<,>]' '{print $5}' | grep zip$ |tail -1)
-#URL="https://sonarsource.bintray.com/Distribution/sonarqube/$VER"
-URL=$(curl https://binaries.sonarsource.com/Distribution/sonarqube/ | grep zip | grep dl_page | grep btn-primary | tail -1 | awk -F \" '{print $2}')
-TFILE="/opt/$(echo $URL |awk -F / '{print $NF}')"
-TDIR=$(echo $TFILE|sed -e 's/.zip//')
-rm -rf /opt/sonarqube
-wget $URL -O $TFILE &>/dev/null
-cd /opt
-unzip $VER &>/dev/null
-mv $TDIR sonarqube 
-if [ $? -eq 0 ]; then
-	success "Successfully Downloaded and Extracted SonarQube"
-else
-	error "Error in Downlading and Extracting SonarQube"
-	exit 1
-fi
-
 ## Configure SonarQube
 sed -i -e '/^sonar.jdbc.username/ d' -e '/^sonar.jdbc.password/ d' -e '/^sonar.jdbc.url/ d' -e '/^sonar.web.host/ d' -e '/^sonar.web.port/ d' /opt/sonarqube/conf/sonar.properties
 sed -i -e '/#sonar.jdbc.username/ a sonar.jdbc.username=sonarqube' -e '/#sonar.jdbc.password/ a sonar.jdbc.password=password' -e '/InnoDB/ a sonar.jdbc.url=jdbc:mysql://localhost:3306/sonarqube?useUnicode=true&amp;characterEncoding=utf8&amp;rewriteBatchedStatements=true&amp;useConfigs=maxPerformance' -e '/#sonar.web.host/ a sonar.web.host=0.0.0.0' /opt/sonarqube/conf/sonar.properties
